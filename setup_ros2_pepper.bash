1f7dff7be819b208d462e25848c9a0d075d08455
Lorena Bajo Rebollo <lbajo9@gmail.com>
2018-10-31 12:18:21 +0100

    setup scripts updated

--- setup_ros2_pepper.bash
+++ setup_ros2_pepper.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-source /home/nao/.ros-root/setup_ros1_pepper.bash
+source /home/nao/System/setup_ros1_pepper.bash
 
-export PYTHONHOME="/home/nao/.ros-root/Python-3.6.1"
+export PYTHONHOME="/home/nao/System/Python-3.6.1"
 export PATH="${PYTHONHOME}/bin:${PATH}"
-export LD_LIBRARY_PATH="/home/nao/.ros-root/ros1_dependencies/lib:${PYTHONHOME}/lib:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="/home/nao/System/ros1_dependencies/lib:${PYTHONHOME}/lib:${LD_LIBRARY_PATH}"
 export ROS_IP=$(ip addr show wlan0 | grep -Po '(?<= inet )([0-9]{1,3}.){3}[0-9]{1,3}')
 export NAO_IP=$ROS_IP
 export ROS_MASTER_URI=http://127.0.0.1:11311
 
-source /home/nao/.ros-root/ros2_inst/local_setup.bash
+source /home/nao/System/ros2_inst/local_setup.bash
